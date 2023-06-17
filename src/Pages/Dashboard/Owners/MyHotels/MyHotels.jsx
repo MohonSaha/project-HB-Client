f8aff9a3fc01f9c182b4507e96fa9e210635f262
MohonSaha <practicewithmohon@gmail.com>
2023-06-18 03:47:36 +0600

    implement my rooms feature

--- src/Pages/Dashboard/Owners/MyHotels/MyHotels.jsx
+++ src/Pages/Dashboard/Owners/MyHotels/MyHotels.jsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import { FaEdit, FaPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import useAuth from "../../../../hooks/useAuth";
 
 
 const MyHotels = () => {
 
-    const {user, loading} = useAuth();
+    const { user, loading } = useAuth();
 
-    const {data: hotels=[], isLoading} = useQuery({
-        queryKey: ['isOwner', user?.email],
+    const { data: hotels = [], } = useQuery({
+        queryKey: ['hotels', user?.email],
 
         enabled: !loading && !!user?.email,
 
@@ -18,11 +20,56 @@ const MyHotels = () => {
         }
     })
 
+    console.log(hotels);
+
 
     return (
         <div>
-            
+            <div className="overflow-x-auto w-full rounded-lg border-[#33525f] border-2 md:-mt-8">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-[#33525f] text-white text-base">
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Update</th>
+                            <th>Add Rooms</th>
+
+                        </tr>
+                    </thead>
+                    <tbody className="text-base font-semibold">
+                        {
+                            hotels.map((item, index) => <tr key={item._id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className=" flex-col items-center">
+                                        <div className="avatar">
+                                            <div className="rounded-lg w-28 h-16">
+                                                <img className="w-28 h-16" src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <p></p>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td>
+                                    <Link state={{ state: item }} to={`/dashboard/myClasses/update`}>
+                                        <button className="btn btn-square hover:text-white hover:bg-[#0C4B65] bg-[#0C4B65] btn-circle text-white hover:transform hover:scale-110"><FaEdit></FaEdit></button>
+                                    </Link>
+
+                                </td>
+                                <td><button className="btn btn-square hover:text-white hover:bg-[#0C4B65] bg-[#0C4B65] btn-circle text-white hover:transform hover:scale-110"><FaPlus></FaPlus></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
+
         </div>
+        // <p></p>
     );
 };
 
