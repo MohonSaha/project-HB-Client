8a0b987c60f2fc5d09bcaf029180a7e0e9beb7d9
MohonSaha <practicewithmohon@gmail.com>
2023-06-18 10:59:06 +0600

    create modal and pass data

---
+++ src/Pages/Dashboard/Owners/ManageRooms/ManageRooms.jsx
@@ -0,0 +1,11 @@
+
+
+const ManageRooms = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManageRooms;
--- src/Pages/Home/Hotels/HotelCard.jsx
+++ src/Pages/Home/Hotels/HotelCard.jsx
@@ -1,18 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
 
 
 const HotelCard = ({ hotel }) => {
 
-    const {name, image, location, details} = hotel;
+    const { name, image, location, details, _id } = hotel;
+
+    const { data: rooms=[]} = useQuery({
+        queryKey: ['rooms'],
+
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/rooms/${_id}`)
+            console.log('is rooms response', res)
+            return res.json()
+        }
+    })
 
     return (
         <div>
+
+{/* 
+            <button className="btn" onClick={() => window.my_modal_3.showModal()}>open modal</button> */}
+            <dialog id="my_modal_3" className="modal">
+                <form method="dialog" className="modal-box">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <h3 className="font-bold text-lg">{rooms.length}</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                </form>
+            </dialog>
+
+
+
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button onClick={() => window.my_modal_3.showModal(_id)} className="btn btn-primary">Available Room</button>
                     </div>
                 </div>
             </div>
