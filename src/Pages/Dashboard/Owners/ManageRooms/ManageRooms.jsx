5b58a8a67a52d3b95f4dc3ba4044a3d2c5b2edbb
MohonSaha <practicewithmohon@gmail.com>
2023-06-18 18:22:57 +0600

    create manage rooms template and fetch data

--- src/Pages/Dashboard/Owners/ManageRooms/ManageRooms.jsx
+++ src/Pages/Dashboard/Owners/ManageRooms/ManageRooms.jsx
@@ -1,9 +1,62 @@
+import { Link } from "react-router-dom";
+import useRooms from "../../../../hooks/useRooms";
+import { FaEdit, FaPlus } from "react-icons/fa";
 
 
 const ManageRooms = () => {
+
+    const [rooms] = useRooms();
+    console.log(rooms);
+
     return (
         <div>
-            
+            <div className="overflow-x-auto w-full rounded-lg border-[#33525f] border-2 md:-mt-8">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-[#33525f] text-white text-base">
+                        <tr>
+                            <th>#</th>
+                            <th>Hotel Name</th>
+                            <th>Room Image</th>
+                            <th>Room Type</th>
+                            <th>Price</th>
+                            <th>Owner</th>
+                            <th>Update</th>
+
+                        </tr>
+                    </thead>
+                    <tbody className="text-base font-semibold">
+                        {
+                            rooms.map((item, index) => <tr key={item._id}>
+                                <td>{index + 1}</td>
+                                <td>{item.hotelName}</td>
+                                <td>
+                                    <div className=" flex-col items-center">
+                                        <div className="avatar">
+                                            <div className="rounded-lg w-28 h-16">
+                                                <img className="w-28 h-16" src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <p></p>
+                                    </div>
+                                </td>
+                                <td>{item.type}</td>
+                                <td className="text-right font-bold">${item.price}</td>
+                                <td>{item.ownerEmail}</td>
+                                <td>
+                                    <Link state={{ state: item }} to={`/dashboard/myClasses/update`}>
+                                        <button className="btn btn-square hover:text-white hover:bg-[#0C4B65] bg-[#0C4B65] btn-circle text-white hover:transform hover:scale-110"><FaEdit></FaEdit></button>
+                                    </Link>
+
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
+
         </div>
     );
 };
--- src/hooks/useRooms.jsx
+++ src/hooks/useRooms.jsx
@@ -1,23 +1,23 @@
-// import { useQuery } from "@tanstack/react-query";
-// import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
 
 
-// const useBooking = () => {
-//     const { user, loading } = useAuth();
+const useRooms = () => {
+    const { user, loading } = useAuth();
 
-//     const { data: bookedRoom = [], refetch } = useQuery({
-//         queryKey: ['rooms', user?.email],
+    const { data: rooms = [], refetch } = useQuery({
+        queryKey: ['rooms', user?.email],
 
-//         enabled: !loading && !!user?.email,
+        enabled: !loading && !!user?.email,
 
-//         queryFn: async () => {
-//             const res = await fetch(`http://localhost:5000/booked/room?email=${user?.email}`)
-//             console.log('is useBooking response', res)
-//             return res.json()
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/rooms/owner?ownerEmail=${user?.email}`)
+            console.log('is userooms response', res)
+            return res.json()
             
-//         },      
-//     })
-//     return [bookedRoom, refetch] 
-// };
+        },      
+    })
+    return [rooms, refetch] 
+};
 
-// export default useBooking;
+export default useRooms;
