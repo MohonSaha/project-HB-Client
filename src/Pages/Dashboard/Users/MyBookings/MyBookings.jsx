ef23506092339eb73f0ced70b62e2104f7670bdb
MohonSaha <practicewithmohon@gmail.com>
2023-06-18 16:17:54 +0600

    implement my bookings page

--- src/Pages/Dashboard/Users/MyBookings/MyBookings.jsx
+++ src/Pages/Dashboard/Users/MyBookings/MyBookings.jsx
@@ -1,9 +1,61 @@
+import { Link } from "react-router-dom";
+import useBooking from "../../../../hooks/useBooking";
+import { FaEdit, FaPlus } from "react-icons/fa";
 
 
 const MyBookings = () => {
+
+    const [bookedRoom] = useBooking();
+    console.log(bookedRoom);
+
+
     return (
         <div>
-            <h2>My Bookings is here</h2>
+            <div className="overflow-x-auto w-full rounded-lg border-[#33525f] border-2 md:-mt-8">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-[#33525f] text-white text-base">
+                        <tr>
+                            <th>#</th>
+                            <th>Hotel</th>
+                            <th>Name</th>
+                            <th>Update</th>
+                            <th>Add Rooms</th>
+
+                        </tr>
+                    </thead>
+                    <tbody className="text-base font-semibold">
+                        {
+                            bookedRoom.map((item, index) => <tr key={item._id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className=" flex-col items-center">
+                                        <div className="avatar">
+                                            <div className="rounded-lg w-28 h-16">
+                                                <img className="w-28 h-16" src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <p></p>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td>
+                                    <Link state={{ state: item }} to={`/dashboard/myClasses/update`}>
+                                        <button className="btn btn-square hover:text-white hover:bg-[#0C4B65] bg-[#0C4B65] btn-circle text-white hover:transform hover:scale-110"><FaEdit></FaEdit></button>
+                                    </Link>
+
+                                </td>
+                                <td>
+                                    <Link state={{ state: item }} to={`/dashboard/myhotels/addRoom`}> <button className="btn btn-square hover:text-white hover:bg-[#0C4B65] bg-[#0C4B65] btn-circle text-white hover:transform hover:scale-110"><FaPlus></FaPlus></button>
+                                    </Link>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/hooks/useBooking.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+
+
+const useBooking = () => {
+    const { user, loading } = useAuth();
+
+    const { data: bookedRoom = [], refetch } = useQuery({
+        queryKey: ['paidClasses', user?.email],
+
+        enabled: !loading && !!user?.email,
+
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/booked/room?email=${user?.email}`)
+            console.log('is useBooking response', res)
+            return res.json()
+            
+        },      
+    })
+    return [bookedRoom, refetch] 
+};
+
+export default useBooking;
